72eeafb93d86baff4fdba394ba7cb993d5822e1e
AlexandrePneuBras <alexandre.rego@pneubras.com>
2026-09-02 15:13:45 -0300

    Ajustando código da logomarca

--- editor_ficha/src/components/PneuBrasLogo.tsx
+++ editor_ficha/src/components/PneuBrasLogo.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
+// O ../../ faz o código sair da pasta components, sair da pasta src e ir para a raiz buscar a imagem
+import logoImg from '../../LogoOriginal.png'; 
 
 interface PneuBrasLogoProps {
   className?: string;
   height?: number;
 }
 
-export const PneuBrasLogo: React.FC<PneuBrasLogoProps> = ({
-  className = '',
-  height = 65,
-}) => {
+export const PneuBrasLogo: React.FC<PneuBrasLogoProps> = ({ className = '', height = 65 }) => {
   return (
-    <img
-      src="/LogoOriginal.png"
+    <img 
+      src={logoImg} 
       alt="Logo Grupo PneuBras"
       style={{ height: `${height}px`, width: 'auto' }}
       className={`object-contain shrink-0 ${className}`}
